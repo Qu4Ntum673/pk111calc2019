b562e6a6fdba7b274ca824d2ce0ac0d39d390c4f
Qu4Ntum673 <98815949+Qu4Ntum673@users.noreply.github.com>
2022-09-22 14:59:12 +0300

    1

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pkk111ilya
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            var a = (Button)sender;
+            textBox1.Text += a.Text; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length>16)
+            {
+                string txt = textBox1.Text;
+                textBox1.Text = txt.Substring(0, 16);
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string txt = textBox1.Text;
+            textBox1.Text = txt.Substring(0, textBox1.Text.Length-1);
+
+        }
+    }
+}
